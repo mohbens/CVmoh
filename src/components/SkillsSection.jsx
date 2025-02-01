826c976d39c93f9f4b1374b26cc0f4ed974b727e
mohbens <mohbenselama@gmail.com>
2025-02-01 23:25:21 +0100

    adding the tools list

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,5 +1,64 @@
 import { Box, Paper, Typography } from "@mui/material";
 import React from "react";
+const tools = [
+	{
+		id: "1",
+		title: "VS Code",
+		img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+	},
+	{
+		id: "2",
+		title: "Command Line",
+		img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg",
+	},
+	{
+		id: "3",
+		title: "Git",
+		img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-plain-wordmark.svg",
+		// Orange variant
+	},
+	{
+		id: "4",
+		title: "npm",
+		img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/npm/npm-original-wordmark.svg",
+		// Red variant
+	},
+	{
+		id: "5",
+		title: "Slack",
+		img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/slack/slack-original.svg",
+		// Purple variant
+	},
+	{
+		id: "6",
+		title: "Photoshop",
+		img: "https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg",
+		// Dark blue Adobe icon
+	},
+	{
+		id: "7",
+		title: "Illustrator",
+		img: "https://upload.wikimedia.org/wikipedia/commons/f/fb/Adobe_Illustrator_CC_icon.svg",
+		// Orange Adobe icon
+	},
+	{
+		id: "8",
+		title: "XD",
+		img: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Adobe_XD_CC_icon.svg",
+		// Pink Adobe icon
+	},
+	{
+		id: "9",
+		title: "Premiere Pro",
+		img: "https://upload.wikimedia.org/wikipedia/commons/4/40/Adobe_Premiere_Pro_CC_icon.svg",
+		// Purple Adobe icon
+	},
+	{
+		id: "10",
+		title: "WebPack",
+		img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-original.svg",
+	},
+];
 
 const skills = [
 	{
@@ -41,13 +100,11 @@ const handleSkills = () => {
 			elevation={10}
 			sx={{
 				padding: 2,
-				// minWidth: 150,
 				margin: 1,
+				transition: "transform 0.2s",
 				borderRadius: "20px",
-
 				"&:hover": {
 					transform: "scale(1.05)",
-					animation: " ease-in-out",
 				},
 			}}>
 			<Box sx={{ display: "flex", alignItems: "center" }}>
@@ -61,6 +118,33 @@ const handleSkills = () => {
 		</Paper>
 	));
 };
+
+const handleTools = () => {
+	return tools.map((tool) => (
+		<Paper
+			key={tool.id}
+			elevation={10}
+			sx={{
+				padding: 2,
+				margin: 1,
+				transition: "transform 0.2s",
+				borderRadius: "20px",
+				"&:hover": {
+					transform: "scale(1.05)",
+				},
+			}}>
+			<Box sx={{ display: "flex", alignItems: "center" }}>
+				<img
+					src={tool.img}
+					alt={tool.title}
+					style={{ width: 40, height: 40 }}
+				/>
+				<Typography variant="h6" sx={{ fontWeight: 500 }}></Typography>
+			</Box>
+		</Paper>
+	));
+};
+
 export default function SkillsSection() {
 	return (
 		<Box sx={{ justifyItems: "center" }}>
@@ -88,9 +172,17 @@ export default function SkillsSection() {
 						display: "flex",
 						flexWrap: "wrap",
 						justifyContent: "center",
-						gap: 3,
 					}}>
 					{handleSkills()}
+				</Box>
+				<Box
+					sx={{
+						width: "50%",
+						display: "flex",
+						flexWrap: "wrap",
+						justifyContent: "center",
+					}}>
+					{handleTools()}
 				</Box>
 			</Box>
 			<hr style={{ width: "75%" }} />
